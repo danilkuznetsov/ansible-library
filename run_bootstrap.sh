7b816297cea73c8d09ee56f9758f89ccff8cf884
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-04-22 16:13:04 +0300

    refactoring main playbook

--- run_bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-##
-## Run the bootstrap.yml  playbook in the configuration/playbooks directory
-##
-
-ansible-playbook bootstrap.yml  -u admin-user -v -i inventory/hosts --ask-become
--- run_create_users.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-##
-## Run the create_users.yml playbook in the configuration/playbooks directory
-##
-
-ansible-playbook create_users.yml -u root -k -v -i inventory/hosts
